1efe17eae578b067b757570a2de8f9eb8b46518e
Oguzhan <oguzhanturgut@outlook.com>
2019-12-24 18:20:33 +0100

    Chapter 3

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
-function Home() {
+import { useEffect } from 'react';
+import axios from 'axios';
+
+function Home(products) {
+  // console.log(products);
+  // const getProducts = async () => {
+  //   const url = 'http://localhost:3000/api/products';
+  //   const response = await axios(url);
+  // };
+
+  // useEffect(() => {
+  //   getProducts();
+  // }, []);
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios(url);
+  return { products: response.data };
+};
 
 export default Home;
