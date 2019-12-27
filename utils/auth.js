be5b7ee6b3f53f51fbe10abf72e3e31030b40ff3
Oguzhan <oguzhanturgut@outlook.com>
2019-12-27 16:53:14 +0100

    Chapter 9

--- utils/auth.js
+++ utils/auth.js
@@ -5,3 +5,18 @@ export const handleLogin = token => {
   cookie.set('token', token);
   Router.push('/account');
 };
+
+export const redirectUser = (ctx, location) => {
+  if (ctx.req) {
+    ctx.res.writeHead(302, { Location: location });
+    ctx.res.end();
+  } else {
+    Router.push(location);
+  }
+};
+
+export const handleLogout = () => {
+  cookie.remove('token');
+  window.localStorage.setItem('logout', Date.now());
+  Router.push('/login');
+};
