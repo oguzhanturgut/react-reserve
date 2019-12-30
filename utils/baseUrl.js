db96c4a9be9afd990f75f823a7a0898310a79761
Oguzhan <oguzhanturgut@outlook.com>
2019-12-30 16:57:49 +0100

    change baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://react-reserve-455.now.sh'
+    ? 'https://ancient-meadow-68017.herokuapp.com/'
     : 'http://localhost:3000';
 
 export default baseUrl;
