16886e4bc3bbfc0199b8981f5a7818de8ca7e599
Oguzhan <oguzhanturgut@outlook.com>
2019-12-30 17:02:16 +0100

    change baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://ancient-meadow-68017.herokuapp.com/'
+    ? 'https://ancient-meadow-68017.herokuapp.com'
     : 'http://localhost:3000';
 
 export default baseUrl;
