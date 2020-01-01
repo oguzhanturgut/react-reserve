de2152d0d5d3e5f079f8cb7c49e78a3af3ee068e
Oguzhan <oguzhanturgut@outlook.com>
2020-01-01 13:06:47 +0100

    Deploy to now.sh

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://ancient-meadow-68017.herokuapp.com'
+    ? // ? 'https://ancient-meadow-68017.herokuapp.com'
+      'https://react-reserve-455.now.sh'
     : 'http://localhost:3000';
 
 export default baseUrl;
